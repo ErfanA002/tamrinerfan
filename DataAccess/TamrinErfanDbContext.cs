24a0754ad3746498df2d68124956ae30f1114e81
Lenovo <erfanasheri2002@gmail.com>
2024-06-09 22:06:03 +0330

    edite efcore

--- DataAccess/TamrinErfanDbContext.cs
+++ DataAccess/TamrinErfanDbContext.cs
@@ -3,6 +3,10 @@ using tamrinerfan.Domain;
 namespace tamrinerfan.DataAccess;
 public class TamrinErfanDbContext : DbContext
 {
+    public TamrinErfanDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
     public DbSet<Person> Persons { get; set; }
     public DbSet<Province> Provinces { get; set; }
     public DbSet<City> Citys { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using tamrinerfan.DataAccess;
+
 namespace tamrinerfan;
 public class Program
 {
@@ -11,6 +14,11 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<TamrinErfanDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ErfanDb"));
+        });
 
         var app = builder.Build();
 
